641c553afaedb80b4c70bb225e696ab425339e15
Samia B <samiaab1990@gmail.com>
2023-08-24 11:31:16 -0400

    new website

---
+++ scripts/index.js
@@ -0,0 +1,37 @@
+let images = [];
+
+for (let i = 1; i <= 4; i++) {
+    const img = new Image(3210,560);
+    img.src = `./anim/anim${i}.png`;
+    images.push(img);
+}
+
+let avis = ['./images/capsule.png','./images/public_health.png','./images/kawaii_graph.png', './images/kawaii_dev.png'];
+let widths = ['5%','7.5%','9%','8.5%']
+let click = 0;
+
+$(document).ready(function()
+{
+ 
+  $("#main-avi").click(function()
+  {
+    click++;
+
+    if(click < avis.length)
+    {
+      $(this).attr({'src': avis[click], 'width': widths[click]});
+    }
+    else
+    {
+      click = 0;
+      $(this).attr({'src': avis[click], 'width': widths[click]});
+    }
+
+  });
+});
+
+function changeAvi(source,width)
+{
+  $("#main-avi").attr({'src': source, 'width': width}); 
+}
+
