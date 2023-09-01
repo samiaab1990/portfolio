5ebb8064892bd76b091b76124f7a1a86277badb7
Samia B <samiaab1990@gmail.com>
2023-09-01 11:38:36 -0400

    test

--- scripts/index.js
+++ scripts/index.js
@@ -19,10 +19,11 @@ for(let i = 0; i <= 4; i++)
   images.push(img);
 }
 
+// avitar change 
 $(document).ready(function()
 {
  
-  // Change avis by clicking 
+  // Change avis by clicking the avatar itself
   $("#main-avi").click(function()
   {
     click++;
@@ -38,6 +39,7 @@ $(document).ready(function()
     }
 
   });
+
 });
 
 // Create a function that changes avi upon link click 
@@ -79,4 +81,92 @@ $(document).ready(function()
     }
   });
 });
+
+
+$(document).ready(function()
+{
+  $($('.navbar a')[0]).addClass('active');
+
+$('.container').on('scroll', function()
+ {
+ $('section').each(function(i)
+ {
+    var section_id = $($('section')[i]).attr('id');
+    var height_threshold = document.getElementById(section_id).offsetTop * .75;
+
+    if($('.container').scrollTop() >= height_threshold)
+    {
+  
+      $('.navbar a').each(function(i)
+      {
+        $($('.navbar a')[i]).removeClass('active');
+      });
+
+      $($('.navbar a')[i]).addClass('active');
+    }
+  
+ });
+  
+});
+});
+
+const abt_divs = [];
+const desc_divs =['About.','Skills.','Hobbies.','This Page.']
+
+$(document).ready(function()
+{ 
+  for(let i = 1; i < $('.about > div').length - 1; i++)
+  {
+    abt_divs.push($($('.about > div')[i]).attr('class'));
+  }
+
+});
+
+$(document).ready(function()
+{ 
+  for(let i = 0; i < abt_divs.length; i++)
+  {
+    if($("." + abt_divs[i]).css('display') === 'block')
+    {
+      $('.div-change-buttons > a:nth-child(' + (i + 1) + ')').css('background-color','var(--font-color)');
+    }
+  }
+
+});
+
+
+function changeDiv(div)
+{
+
+  $("." + div).css({'display':'block','animation':'linear-transition 1s'});
+
+  for(let i = 0; i < abt_divs.length; i++)
+  {
+    if(div != abt_divs[i])
+    {
+        $("." + abt_divs[i]).css({'display':'none'});
+        $('.div-change-buttons > a:nth-child(' + (i + 1) + ')').css('background-color','transparent');
+    }
+    else
+    {
+      $('.div-change-buttons > a:nth-child(' + (i + 1) + ')').css('background-color','var(--font-color)');
+      $('#secondary-heading').html(desc_divs[i]);
+
+      if(i + 1 < abt_divs.length)
+      {
+        const div_string = desc_divs[i + 1].substring(0 , desc_divs[i + 1].length - 1); 
+        $('.next-page').attr("onclick","changeDiv('" + abt_divs[i + 1]+"');");
+        $('.next-page > span').html(div_string);
+      }
+      else
+      {
+        const div_string = desc_divs[0].substring(0 , desc_divs[0].length - 1); 
+        $('.next-page').attr("onclick","changeDiv('" + abt_divs[0]+"');");
+        $('.next-page > span').html(div_string);
+      }
+
+     
+    }
+  }
+}
 
